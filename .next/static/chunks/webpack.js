f3d9a92fd98f322fc5647cce7167608f2009c144
A.B. [Abdulbosit] <abdulbosit.melikuziev@gmail.com>
2025-08-08 01:19:00 +0100

    Fix email overflow in contact card

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "03bf218dd59e7cb1"; }
+/******/ 		__webpack_require__.h = function() { return "5eafa47ffe7f8154"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
